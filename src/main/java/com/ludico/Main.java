e4b1714fac9e3871716680eedf6325126a5c051c
Walter Cavalcante <cavalcantewalter665@gmail.com>
2024-09-24 14:35:51 -0300

    inicio_maven

---
+++ src/main/java/com/ludico/Main.java
@@ -0,0 +1,11 @@
+package com.ludico;
+
+import javafx.application.Application;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)  {
+
+
+    }
+}
